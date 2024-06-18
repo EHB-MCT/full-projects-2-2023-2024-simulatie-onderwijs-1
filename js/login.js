3fe43772c9dc3b366611083ac3ed60706c9260c5
debbiewillekens <145569137+debbiewillekens@users.noreply.github.com>
2024-06-18 21:14:45 +0200

    cookies voor loginfuncties toegevoegd

--- js/login.js
+++ js/login.js
@@ -1,25 +1,50 @@
 const form = document.getElementById("login-form");
 const errorMessage = document.getElementById("error-message");
 const loginButton = document.getElementById("inloggen");
+const loggedIn = document.cookie.includes("cookieAccepted=true")
+	? localStorage.getItem("loggedIn")
+	: null;
 const logoutbutton = document.getElementById("uitloggen");
 
-form.addEventListener("submit", (event) => {
-	const username = document.getElementById("username").value;
-	const password = document.getElementById("password").value;
+if (window.location.href.endsWith("login.html")) {
+	form.addEventListener("submit", (event) => {
+		const username = document.getElementById("username").value;
+		const password = document.getElementById("password").value;
 
-	if (username === "admin" && password === "admin") {
-		localStorage.setItem("loggedIn", true);
-		window.location.href = "index.html";
-	} else {
-		localStorage.setItem("loggedIn", false);
-		event.preventDefault(); // Prevent form submission
-		errorMessage.textContent =
-			"Gelieve gebruikersnaam en wachtwoord correct in te voeren.";
-	}
-});
+		if (username === "admin" && password === "admin") {
+			localStorage.setItem("loggedIn", true);
+			window.location.href = "index.html";
+		} else {
+			localStorage.setItem("loggedIn", false);
+			event.preventDefault(); // Prevent form submission
+			errorMessage.textContent =
+				"Gelieve gebruikersnaam en wachtwoord correct in te voeren.";
+		}
+	});
+}
 
 // Check if user is logged in and hide/show login button accordingly
 window.onload = function () {
+	// Prompt user to accept cookies
+	const cookieConsent = document.getElementById("cookieConsent");
+	if (cookieConsent) {
+		cookieConsent.addEventListener("click", () => {
+			document.cookie = `cookieAccepted=true; path=/`;
+			cookieConsent.style.display = "none";
+		});
+	}
+
+	// Make cookies available for this site
+	function getCookie(name) {
+		const value = `; ${document.cookie}`;
+		const parts = value.split(`; ${name}=`);
+		if (parts.length === 2) return parts.pop().split(";").shift();
+	}
+
+	if (getCookie("cookieAccepted") !== "true") {
+		cookieConsent.style.display = "block";
+	}
+
 	if (loggedIn === "true") {
 		loginButton.style.display = "none";
 		logoutbutton.style.display = "block";
